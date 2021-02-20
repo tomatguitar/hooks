ec2588e7632f65da8991798f9985d9fe5d252b12
Dmytro Zhyltsov <zkarman@mail.ru>
2021-02-20 13:32:04 +0200

    feat: add example with useState hook

--- src/index.js
+++ src/index.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const El = () => {
-   return <h1>Hello World!</h1>
-}
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />;
+    </div>
+  );
+};
+
+const HookSwitcher = () => {
+  const [color, setColor] = useState('white');
+  const [fontSize, setFontSize] = useState(14);
+  return (
+    <div
+      style={{
+        padding: '10px',
+        backgroundColor: color,
+        fontSize: `${fontSize}px`,
+      }}
+    >
+      <h1>TEXT</h1>
+      <button
+        onClick={() => {
+          setColor('black');
+        }}
+      >
+        Dark
+      </button>
+      <button
+        onClick={() => {
+          setColor('white');
+        }}
+      >
+        Light
+      </button>
+      <button
+        onClick={() => {
+          setFontSize((prevFontSize) => prevFontSize + 7);
+        }}
+      >
+        Bigger
+      </button>
+      <button
+        onClick={() => {
+          setFontSize((prevFontSize) => prevFontSize - 7);
+        }}
+      >
+        Smaller
+      </button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <El />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
